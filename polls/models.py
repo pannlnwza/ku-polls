80d2169887aedc68190a45a009bec504af7553c4
Pattapon Gowanit <iampattapon@gmail.com>
2024-08-24 23:24:18 +0700

    Implement tutorial part 5

--- polls/models.py
+++ polls/models.py
@@ -28,7 +28,8 @@ class Question(models.Model):
         Returns:
             bool: True if the question was published within the last day, False otherwise.
         """
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
